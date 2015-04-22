a09802f4aa14c1a8052fac2fb933660923209d6a
kvvzr <kwzr@twinkrun.net>
2015-04-22 18:45:32 +0900

    Initial Commit

---
+++ TrackpadSIF/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TrackpadSIF
+//
+//  Created by Kawazure on 2015/04/22.
+//  Copyright (c) 2015年 kwzrhouse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TrackpadSIF/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrackpadSIF
+//
+//  Created by Kawazure on 2015/04/22.
+//  Copyright (c) 2015年 kwzrhouse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
